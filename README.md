f34103bafa7c3ab421944c5f36b36fa7b41f0781
Simon Anthony <github@torty.org.uk>
2026-07-21 09:17:00 +0100

    Proms Lister 5.0 - Sync Edition

---
+++ README.md
@@ -0,0 +1,10 @@
+# Proms Lister Sync Edition
+
+Single-device local ticket storage with ZIP export/import for synchronising between desktop and iPhone.
+
+- Drag and drop Royal Albert Hall ticket PDFs
+- Ticket flags on the Prom list
+- Click ticket flag to open the PDF
+- Full programme details and editable ticket notes
+- Export ZIP includes ticket details and PDFs
+- Import ZIP restores them on another device
---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'proms-finder-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon.png'];
+
+self.addEventListener('install', evt => {
+  evt.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(()=>{}));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cached => cached || fetch(evt.request).then(res => {
+      const resClone = res.clone();
+      caches.open(CACHE).then(c => c.put(evt.request, resClone));
+      return res;
+    }).catch(() => cached))
+  );
+});
