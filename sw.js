b70fc24ca402a8a339cee43ecbc36dbedfa91ba9
Simon Anthony <github@torty.org.uk>
2026-08-09 05:35:58 +0100

    Reset to Proms-Lister-VERY-GOOD-plus-ticket-page-seat-FLAT 3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'proms-finder-v2';
+const CACHE = 'proms-finder-v8-very-good-page-seat';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.png'];
 
 self.addEventListener('install', evt => {
