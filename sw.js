7604bed05bb2b79254aea6cf2362b715a1e9942f
Simon Anthony <github@torty.org.uk>
2026-08-10 08:23:48 +0100

    Activate live slide-in menus without altering EXCELLENT app logic

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'proms-finder-v10-excellent-ui-refresh';
-const ASSETS = ['./', './index.html', './manifest.json', './rah-seating-plan.js', './rah-seating-plan2.jpeg', './data.json'];
+const CACHE = 'proms-finder-v12-live-slide-menu';
+const ASSETS = ['./', './index.html', './manifest.json', './rah-seating-plan.js', './rah-seating-plan2.jpeg', './data.json', './slide-menu-live.js'];
 
 self.addEventListener('install', evt => {
   evt.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(()=>{}));
@@ -7,18 +7,22 @@ self.addEventListener('install', evt => {
 });
 
 self.addEventListener('activate', evt => {
-  evt.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
-  );
+  evt.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
   self.clients.claim();
 });
 
 self.addEventListener('fetch', evt => {
-  evt.respondWith(
-    caches.match(evt.request).then(cached => cached || fetch(evt.request).then(res => {
-      const resClone = res.clone();
-      caches.open(CACHE).then(c => c.put(evt.request, resClone));
-      return res;
-    }).catch(() => cached))
-  );
+  if (evt.request.mode === 'navigate') {
+    evt.respondWith(fetch(evt.request).then(async res => {
+      const type=res.headers.get('content-type')||'';
+      if(!type.includes('text/html')) return res;
+      let html=await res.text();
+      if(!html.includes('slide-menu-live.js')) html=html.replace('</body>','<script src="slide-menu-live.js?v=eadb4400"></script></body>');
+      return new Response(html,{status:res.status,statusText:res.statusText,headers:res.headers});
+    }).catch(()=>caches.match('./index.html')));
+    return;
+  }
+  evt.respondWith(caches.match(evt.request).then(cached => cached || fetch(evt.request).then(res => {
+    const copy=res.clone(); caches.open(CACHE).then(c=>c.put(evt.request,copy)); return res;
+  }).catch(()=>cached)));
 });
